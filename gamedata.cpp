a7bc89b98cf6d280911b17e29bbe8aa0ad06675e
FrostHand <dv_frost@mail.ru>
2017-11-30 19:41:34 +0300

    Added animation sequence to game resources

--- gamedata.cpp
+++ gamedata.cpp
@@ -1,6 +1,7 @@
 #include "gamedata.h"
 #include <QtWidgets>
 #include <cassert>
+#include <sstream>
 
 ResourceWrapper::ResourceWrapper(const char * path)
     :path(path)
@@ -21,5 +22,23 @@ GameData::GameData()
     assert(!img_hor.isNull() && !img_ver.isNull());
 
     this->blockSize = img_hor.width();
+
+    // Loading animation frames for rotations from 0 to 90 degrees
+    // Caution: this anim frames are hardcoded
+    for(int i = 0; i < 9; i++)
+    {
+        std::stringstream ss;
+        ss<<":data/anim"<<i;
+        this->hor_to_ver.push_back(QImage(ss.str().c_str()));
+    }
+
+    // Loading animation frames for rotations from 90 to 180 degrees
+    // Caution: this anim sequence is hardcoded
+    for(int i = 9; i < 18; i++)
+    {
+        std::stringstream ss;
+        ss<<":data/anim"<<i;
+        this->ver_to_hor.push_back(QImage(ss.str().c_str()));
+    }
 }
 
